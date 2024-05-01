96e5a2f0a63e0f19cbc18d0e4152b731a630ab95
raman kumar <ramankrchoudhary11@gmail.com>
2024-05-01 14:16:59 +0530

    some minor changes

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib import admin
-from .views import BannerViewSet,UserViewSet,NewsEventsViewSet,OpportunitiesViewSet,GalleryViewSet
+from .views import BannerViewSet,UserViewSet,NewsEventsViewSet,OpportunitiesViewSet,NewsLetterViewSet,GalleryViewSet
 
 admin.site.site_header = "IRFPS Admin"
 admin.site.site_title = "IRFPS Admin Portal"
@@ -12,5 +12,5 @@ urlpatterns = [
     path('news-event/', NewsEventsViewSet.as_view()),
     path('opportunities/', OpportunitiesViewSet.as_view()),
     path('gallery/', GalleryViewSet.as_view()),
-    path('news-letter/', GalleryViewSet.as_view())
+    path('news-letter/', NewsLetterViewSet.as_view())
 ]
